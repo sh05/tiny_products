b54eb1ada85a801b35f0232faee87be759665222
sh05 <shogonakamoto0107@gmail.com>
2020-11-09 23:12:00 +0900

    allow cors

--- hee_chat/main.py
+++ hee_chat/main.py
@@ -9,13 +9,13 @@ from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # CORSを回避するために追加
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,   # 追記により追加
-#     allow_methods=["*"],      # 追記により追加
-#     allow_headers=["*"]       # 追記により追加
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 
 
 DIR = "/app/dist/"
